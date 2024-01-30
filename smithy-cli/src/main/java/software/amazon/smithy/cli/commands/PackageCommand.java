ad496345a83260e67a6c814b93958d766f044361
Hunter Mellema <hpm@amazon.com>
2024-01-30 15:17:10 -0700

    Add support for adding sources to jars

--- smithy-cli/src/main/java/software/amazon/smithy/cli/commands/PackageCommand.java
+++ smithy-cli/src/main/java/software/amazon/smithy/cli/commands/PackageCommand.java
@@ -15,11 +15,24 @@
 
 package software.amazon.smithy.cli.commands;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import software.amazon.smithy.build.FileManifest;
@@ -33,12 +46,16 @@ import software.amazon.smithy.cli.Arguments;
 import software.amazon.smithy.cli.CliError;
 import software.amazon.smithy.cli.Command;
 import software.amazon.smithy.cli.HelpPrinter;
+import software.amazon.smithy.cli.SmithyCli;
 import software.amazon.smithy.cli.dependencies.DependencyResolver;
 import software.amazon.smithy.cli.dependencies.ResolvedArtifact;
 
 final class PackageCommand implements Command {
     private static final Logger LOGGER = Logger.getLogger(PackageCommand.class.getName());
-
+    private static final String SOURCES = "sources";
+    private static final String SMITHY_SOURCE_PREFIX= "META-INF/smithy/";
+    private static final int BUFFER_SIZE = 1024;
+    private static final String BUILD_TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
     private final DependencyResolver.Factory dependencyResolverFactory;
     private final String parentCommandName;
 
@@ -135,7 +152,7 @@ final class PackageCommand implements Command {
                                    Path outputDirectory,
                                    List<ResolvedArtifact> runtimeDeps
     ) {
-        Path sourcesPath = outputDirectory.resolve(name).resolve("sources");
+        Path sourcesPath = outputDirectory.resolve(name).resolve(SOURCES);
         if (!Files.exists(sourcesPath)) {
             throw new CliError("Could not find generated artifacts for projection `" + name
                     + "`. Run `smithy build` before attempting to package build artifacts.");
@@ -144,8 +161,7 @@ final class PackageCommand implements Command {
         validateConfig(config, name);
 
         // Get a file manifest scoped to the ${outputDir}/${projection}/release/jar/
-        FileManifest pluginPackagingManifest = FileManifest.create(
-                outputDirectory.resolve(name).resolve(PACKAGING_OUTPUT));
+        FileManifest pluginPackagingManifest = FileManifest.create(outputDirectory.resolve(name).resolve(PACKAGING_OUTPUT));
 
         // Write pom file
         PomFile pom = new PomFile(config.getArtifactId().get(), config.getGroupId().get(), config.getVersion().get());
@@ -154,20 +170,85 @@ final class PackageCommand implements Command {
         pom.write(pluginPackagingManifest);
 
         // TODO: add trait codegen?
-        // Write Jar file with just sources
-
+        Path jarFilePath = pluginPackagingManifest.addFile(Paths.get(
+                config.getArtifactId().get() + "-" + config.getVersion().get() + ".jar"));
+        try (JarOutputStream jos = new JarOutputStream(Files.newOutputStream(jarFilePath), getJarManifest(config))) {
+            for (Path srcsPath : listContents(sourcesPath.toFile())) {
+                LOGGER.warning("FOUND SOURCES PATH " + srcsPath);
+                String jarPath = getJarPath(sourcesPath, SMITHY_SOURCE_PREFIX, srcsPath);
+                LOGGER.warning("TRANSLATED TO JAR PATH " + jarPath);
+                writeJarEntry(jarPath, srcsPath, jos);
+            }
+        } catch (IOException exc) {
+            throw new UncheckedIOException(exc);
+        }
 
         LOGGER.warning("PACKAGING PROJECTION: " + name);
     }
 
+    private static void writeJarEntry(String fileName, Path path, JarOutputStream target) throws IOException {
+        try (InputStream is = Files.newInputStream(path)) {
+            writeJarEntry(fileName, is, target);
+        }
+    }
 
-    private void validateConfig(PackagingConfig config, String projectionName) {
+    private String getJarPath(Path root, String prefix, Path src) {
+        return prefix + root.relativize(src);
+    }
+
+    public List<Path> listContents(File directory) {
+        LOGGER.warning("LISTING FOR " + directory);
+        List<Path> contents = new ArrayList<>();
+        for (final File fileEntry : Objects.requireNonNull(directory.listFiles())) {
+            if (fileEntry.isDirectory()) {
+                contents.addAll(listContents(fileEntry));
+            } else {
+                contents.add(fileEntry.toPath());
+            }
+        }
+        return contents;
+    }
+
+    private static void writeJarEntry(String fileName, InputStream inputStream, JarOutputStream target)
+            throws IOException {
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead;
+
+        target.putNextEntry(new JarEntry(fileName));
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            target.write(buffer, 0, bytesRead);
+        }
+        target.closeEntry();
+    }
+
+
+    private static void validateConfig(PackagingConfig config, String projectionName) {
         config.getVersion().orElseThrow(
                 () -> new CliError("`version` not configured for projection `" + projectionName + "`."));
         config.getGroupId().orElseThrow(
                 () -> new CliError("`groupId` not configured for projection `" + projectionName + "`."));
         config.getArtifactId().orElseThrow(
                 () -> new CliError("`artifactId` not configured for projection `" + projectionName + "`."));
+    }
+
+
+    private static Manifest getJarManifest(PackagingConfig config) {
+        final Manifest manifest = new Manifest();
+        Attributes attributes = manifest.getMainAttributes();
+        attributes.put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        attributes.put(new Attributes.Name("Build-Timestamp"),
+                new SimpleDateFormat(BUILD_TIMESTAMP_FORMAT).format(new Date()));
+        attributes.put(new Attributes.Name("Created-With"), "Smithy-Jar-Plugin (" + SmithyCli.getVersion() + ")");
+
+        if (!config.getTags().isEmpty()) {
+            attributes.put(new Attributes.Name("Smithy-Tags"), String.join(",", config.getTags()));
+        }
+
+        for (Map.Entry<String, String> headerEntry : config.getManifestHeaders().entrySet()) {
+            attributes.put(new Attributes.Name(headerEntry.getKey()), headerEntry.getValue());
+        }
+
+        return manifest;
     }
 
     private List<ResolvedArtifact> resolveRuntimeDependencies(SmithyBuildConfig smithyBuildConfig,
